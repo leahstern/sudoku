cde19d3ffd01b68cdb4d23bc8d0c42f666e94439
Teague Hopkins <teague@teaguehopkins.com>
2017-04-29 14:57:04 -0400

    Rewrite read function with async/await

--- files.js
+++ files.js
@@ -1,8 +1,21 @@
   var Promise = require('bluebird');
   var fs = Promise.promisifyAll(require('fs'));
-
+/*
   exports.read = function read(filename){
     fs.readFileAsync(filename, 'UTF-8').then(function(data){
       console.log(data)
+      //return data
+    })
+  }
+*/
+  exports.read = async function read(filename){
+    let data = await fs.readFileAsync(filename, 'UTF-8');
+      console.log(data)
+      //return data
+    }
+
+  exports.write = function write(filename,data){
+    fs.writeFileAsync(filename, data).then(function(data){
+      //written to disk
     })
   }
